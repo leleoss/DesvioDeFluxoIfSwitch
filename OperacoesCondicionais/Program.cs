b9a719c4b81890d0c6db0192b293ebdfcfc13920
LEONARDO SILVERIO DELA TORRE CERQUEIRA <leonardo.sdtcerqueir@senacsp.edu.br>
2018-04-19 11:13:34 -0300

    Declaração de variáveis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,15 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Comentário in-line(em linha)
+            /*
+                Comentário in-block(em bloco)
+             */
+             //Declaração de variável operação. Ela será do tipo interira
+             int operacao;
+
+             //Declaração das variáveis n1 e n2. Elas serão do tipo double
+             double n1, n2;
         }
     }
 }
